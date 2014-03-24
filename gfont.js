6e92d6cd6909cc859ac6b997cc222a2cace0a3e5
Ke, Mingze <mingze.ke@gmail.com>
2014-03-24 17:57:07 +0800

    add widget

--- gfont.js
+++ gfont.js
@@ -1,47 +1,6 @@
-(function () {
-  var gfont = {
-    template: '<div id="{{id}}" class="{{class}}" size="{{size}}" family="{{family}}"><content></content></div>',
-    script: function () {
-      //default font
-      this.family = 'Freckle Face';
-      this.size = '128px';
-      this.weight = '400';
-      this.shadow = '4px 4px 4px #aaa';
-
-      this.init = function () {
-        this.css('font-family', 'family');
-        this.css('font-size', 'size');
-        this.css('font-weight', 'weight');
-        this.css('text-shadow', 'shadow');
-        this.loadCSS(this.family.replace(/ +/, "+"));
-      };
-
-      this.css = function (cssKey, gkAttr) {
-        if (this.$ele.attr(gkAttr)) {
-          this[gkAttr] = this.$ele.attr(gkAttr);
-        }
-        var cssSet = {};
-        cssSet[cssKey] = this[gkAttr];
-        this.$ele.css(cssSet);
-      };
-
-      this.loadCSS = function (fontFamily) {
-        var url = 'http://fonts.googleapis.com/css?family=' + fontFamily;
-        var link = document.createElement("link");
-        link.type = "text/css";
-        link.rel = "stylesheet";
-        link.href = url;
-        document.getElementsByTagName("head")[0].appendChild(link);
-      };
-    }
-  },
-    factory = function () {
-      $.gk.registry('gfont', gfont);
-    };
-
-  if (typeof define === 'function' && define.amd) {
-    define(factory);
-  } else {
-    factory();
-  }
-}());
+({
+  'name': 'gfont',
+  'imgfile': 'images/gfont.png',
+  'data': '<div is="gfont" size="36px" >Google Font</div>',
+  'components': ['gfont.html']
+})
