0566b034d5cb816d747f91b9dc4dc771a4acef6e
Ke, Mingze <mingze.ke@gmail.com>
2014-04-07 14:28:52 +0800

    rename to gk-google-font

--- gfont.js
+++
@@ -1,6 +0,0 @@
-({
-  'name': 'gfont',
-  'imgfile': 'images/gfont.png',
-  'data': '<div is="gfont" size="36px" >Google Font</div>',
-  'components': ['gfont.html']
-})
---
+++ gk-google-font.js
@@ -0,0 +1,6 @@
+({
+  'name': 'gk-google-font',
+  'imgfile': 'images/gk-google-font.png',
+  'data': '<div is="gk-google-font" size="36px" >Google Font</div>',
+  'components': ['gk-google-font.html']
+})
